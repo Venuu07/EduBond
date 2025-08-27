53ea7deb53b0c78d82931386dfe9a13ba93e18ee
Venuu07 <venuchinthakunta7@gmail.com>
2025-08-27 08:16:59 +0530

    Feat: Set up basic Express server

---
+++ server/server.js
@@ -0,0 +1,18 @@
+
+const express = require('express');
+
+
+const app = express();
+
+
+const PORT = 5000;
+
+
+app.get('/', (req, res) => {
+  res.send('Hello from the EduBond Backend!');
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
