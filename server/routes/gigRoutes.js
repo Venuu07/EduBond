48934959d0e71eac16554d0dd08a55e280137074
Venuu07 <venuchinthakunta7@gmail.com>
2025-09-18 12:24:48 +0530

    add application into gig schema

--- server/routes/gigRoutes.js
+++ server/routes/gigRoutes.js
@@ -1,13 +1,19 @@
+
+
 import express from 'express';
 const router=express.Router();
 import {
     getGigs,
     createGig,
     getGigById,
+    getMyGigs,
+    applyToGig,
 } from '../controllers/gigController.js'
 import { protect } from '../middleware/authMiddleware.js';
 
 router.route('/').get(getGigs).post(protect,createGig);
+router.route('/mygigs').get(protect,getMyGigs)
 router.route('/:id').get(getGigById);
+router.route('/:id/apply').post(protect,applyToGig);
 
 export default router;
