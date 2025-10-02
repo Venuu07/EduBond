c099065dd64d011f28f5bc343e7b1148138e6f90
Venuu07 <venuchinthakunta7@gmail.com>
2025-10-02 17:35:57 +0530

    feat: integrate socket.io for real-time messaging functionality

--- server/server.js
+++ server/server.js
@@ -2,8 +2,12 @@
 
 import dotenv from 'dotenv';
 dotenv.config();
+
 import express from 'express';
 import cors from 'cors'; 
+import http from 'http';
+import { Server } from 'socket.io';
+
 import connectDB from './config/db.js'
 import userRoutes from './routes/userRoutes.js'
 import errorHandler from './middleware/errorMiddleware.js';
@@ -14,6 +18,14 @@ connectDB();
 
 const app = express();
 
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000", 
+    methods: ["GET", "POST"]
+  }
+});
 app.use(express.json());
 app.use(cors());
 
@@ -25,13 +37,24 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/users',userRoutes)
-
 app.use('/api/gigs',gigRoutes);
-
 app.use('/api/exchanges',skillExchangeRoutes);
 
 app.use(errorHandler)
 
-app.listen(PORT, () => {
+io.on('connection', (socket) => {
+  console.log(`A user connected:, ${socket.id}`);
+
+  socket.on('sendMessage', (data) =>{
+
+    io.emit('receiveMessage', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`User disconnected: ${socket.id}`);
+  });
+});
+
+server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
