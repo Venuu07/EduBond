09b62d0f0adf05bb1963561ab0ac938ff006d4fd
Venuu07 <venuchinthakunta7@gmail.com>
2025-10-19 12:03:23 +0530

    fix: comment out debug log statements in mentorship routes

--- server/routes/mentorshipRoutes.js
+++ server/routes/mentorshipRoutes.js
@@ -13,12 +13,12 @@ import { protect } from '../middleware/authMiddleware.js';
 router.route('/').get(getSessions).post(protect, createSession);
 
 router.route('/mybookings').get(protect, (req, res, next) => {
-  console.log('HIT /mybookings route'); // Log when this specific route is hit
+//   console.log('HIT /mybookings route'); // Log when this specific route is hit
   getMyBookedSessions(req, res, next); // Then pass control to the actual controller
 });
 
 router.route('/myoffers').get(protect, (req, res, next) => {
-  console.log('HIT /myoffers route'); // Log when this specific route is hit
+//   console.log('HIT /myoffers route'); // Log when this specific route is hit
   getMyOfferedSessions(req, res, next); // Then pass control to the actual controller
 })
 
